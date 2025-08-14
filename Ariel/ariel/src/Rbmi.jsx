011a03cee0dc36b55e9459ad6e8d5b6a8b401ba6
Suresh Rajpoot <imssr1005@gmail.com>
2025-08-14 10:52:54 +0530

    Refactor Rbmi component to use numeric state and improve button functionality

--- Ariel/ariel/src/Rbmi.jsx
+++ Ariel/ariel/src/Rbmi.jsx
@@ -1,35 +1,64 @@
 import { useState } from "react";
 
 
-
 function Rbmi() {
-    const [data, setData] = useState("logged in")
-    const{email,password}=data
+    // let[data,setData]=useState("content")
+     let[data,setData]=useState (0)
 
-    const handlecharge=(e)=>{
+// let data="content"
+console.log(data);
+const clicking=()=>{
+    setData("something")
+    // data="something"
+    alert("clikeed")
+}
+    // const [data, setData] = useState("logged in")
+    // const{email,password}=data
+
+    // const handlecharge=(e)=>{
 
     
-      const{name,value}=()=>{
-        setData({...data,
-        [name]:value
-        })
-    }
-  }
+    //   const{name,value}=()=>{
+    //     setData({...data,
+    //     [name]:value
+    //     })
+    // }
+  
 
-    const handleclick = () => {
-        data1 = "Loggend In "
-        console.log(data1);
-    }
-    const li = ["apple", "ball", "cat"]
-    let data1 = "Are you REndering  "
+    // const handleclick = () => {
+    //     data1 = "Loggend In "
+    //     console.log(data1);
+    // }
+    // const li = ["apple", "ball", "cat"]
+    // let data1 = "Are you REndering  "
 
-    const obj = { id: 1, "name": "suresh", "College": "RBCET" }
-    const handleSubmit=(e)=>{
-      e.preventDefault()
-     email.trim()?alert("email enter"):alert("pls enter the vaild Email")
+    // const obj = { id: 1, "name": "suresh rajpoot", "College": "RBCET" }
+    // const handleSubmit=(e)=>{
+    //   e.preventDefault()
+    //  email.trim()?alert("email enter"):alert("pls enter the vaild Email")
+    // }
+    const increase = () => {
+
+        setData((next)=>next + 1)
+        setData((next)=>next + 1)
+       
+    }
+    const decrease = () => {
+        setData((prev)=>prev - 1)
+        setData((prev)=>prev-1)
+        // setData(data - 1)
+        //   setData(data - 1)
     }
     return (
         <>
+        <div> {data}</div>
+        <br/>
+        <button onClick={decrease}>Decrease by 1</button>
+        <button onClick={increase}>increse by 1</button>
+
+        {/* <button onClick={} style={{border:"solid red 2px"}}> onClick</button> */}
+
+        
             {/* <h1>{data1}</h1>
 
             <button onClick={handleclick}>logout</button>
@@ -44,12 +73,12 @@ function Rbmi() {
                     return <li key={index}>{element}</li>
                 })}
             </ul> */}
-           <form onSubmit={handleSubmit}>
+           {/* <form onSubmit={handleSubmit}>
             <input  type="email" placeholder="email" required value={email} onChange={handlecharge} /> 
             <input type="password" placeholder=" password"required value={password} onChange={handlecharge} /> 
             <input type="submit"/>
            </form>
-           
+            */}
          
            
             {/* <ul>
@@ -57,7 +86,9 @@ function Rbmi() {
                     return <li key={key}>{key}: {value}</li>
                 })}
             </ul> */}
+
         </>
+
     );
 }
 export default Rbmi;
